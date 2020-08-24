fd38a29b641930c08e9a869fda6644c3bc2f2f5e
JustLuuk <151666@csgaugustinus.nl>
2020-08-24 10:57:22 +0000

    veel

--- JS/test.js
+++ JS/test.js
@@ -1,14 +1,19 @@
+var i;
+
+function preload() {
+pl = loadImage("../images/sprites/kever.png");
+}
 function setup() {
   canvas = createCanvas(450,450);
   background('silver');
+  textSize(30);
   canvas.parent('processing');
-  //noLoop();
 }
 
 function draw() {
-  noStroke();
-  fill('steelblue');
-  ellipse(0,0,800);
-  fill('deepskyblue');
-  ellipse(450,450,400);
+  image(pl,0,45,50,50);
+  text("breedte= "+pl.width,150,40);
+  for(i - 0; i < 10; i++) {
+      
+  }
 }
