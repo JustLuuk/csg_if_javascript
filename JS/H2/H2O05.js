67350ee7eff3204bdbbf83b2f2fd72a93b69a464
JustLuuk <151666@csgaugustinus.nl>
2020-08-30 15:27:34 +0000

    veel

--- JS/H2/H2O05.js
+++ JS/H2/H2O05.js
@@ -31,8 +31,10 @@ function tekenRaster() {
   HINT: je kunt terugkijken naar het raster dat je in H1 hebt gemaakt.
   Maak gebruik van de variabelen die bovenaan zijn gedeclareerd.
   */
-  
-      rect(4*celGrootte,2*celGrootte,celGrootte,celGrootte);
-
+  for (var n = 0;n < aantalRijenRaster;n++) {
+      for (var k = 0;k < aantalKolommenRaster;k++) {
+        rect(k*celGrootte,n*celGrootte,celGrootte,celGrootte);
+      }
+  }
   pop();
 }
