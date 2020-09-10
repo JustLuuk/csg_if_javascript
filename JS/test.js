695b1972f7228dbd25ee6eb2740fbed6bdcdb6d2
JustLuuk <151666@csgaugustinus.nl>
2020-09-10 14:05:38 +0000

    veel

--- JS/test.js
+++ JS/test.js
@@ -1,19 +1,76 @@
-var i;
+var speed = 5;
+var bol = {
+    X: 225,
+    Y: 225,
+    D: 50,
 
-function preload() {
-pl = loadImage("../images/sprites/kever.png");
+
+    beweeg() {
+        if (keyIsDown(LEFT_ARROW)) {
+            this.X -= speed;
+        }
+        if (keyIsDown(RIGHT_ARROW)) {
+            this.X += speed;
+        }
+        if (keyIsDown(UP_ARROW)) {
+            this.Y -= speed;
+        }
+
+        if (keyIsDown(DOWN_ARROW)) {
+            this.Y += speed;
+        }
+        this.X = constrain(this.X, this.D / 2, canvas.width - this.D / 2);
+        this.Y = constrain(this.Y, this.D / 2, canvas.width - this.D / 2);
+    },
+    teken() {
+        fill('blue');
+        noStroke();
+        ellipse(this.X, this.Y, this.D);
+    }
 }
+var bol2 = {
+    X: 225,
+    Y: 225,
+    D: 50,
+
+
+
+    beweeg() {
+        if (keyIsDown(65)) {
+            this.X -= speed;
+        }
+        if (keyIsDown(68)) {
+            this.X += speed;
+        }
+        if (keyIsDown(87)) {
+            this.Y -= speed;
+        }
+
+        if (keyIsDown(83)) {
+            this.Y += speed;
+        }
+        this.X = constrain(this.X, this.D / 2, canvas.width - this.D / 2);
+        this.Y = constrain(this.Y, this.D / 2, canvas.width - this.D / 2);
+    },
+    teken() {
+        fill('red');
+        noStroke();
+        ellipse(this.X, this.Y, 50, 50);
+    }
+}
+
 function setup() {
-  canvas = createCanvas(450,450);
-  background('silver');
-  textSize(30);
-  canvas.parent('processing');
+    canvas = createCanvas(1000, 1000);
+    canvas.parent('processing');
+
+}
+function draw() {
+    background('silver');
+    bol.beweeg();
+    bol.teken();
+    bol2.beweeg();
+    bol2.teken();
+
 }
 
-function draw() {
-  image(pl,0,45,50,50);
-  text("breedte= "+pl.width,150,40);
-  for(i - 0; i < 10; i++) {
-      
-  }
-}
+
