088b4acfb0a97f1b5162b39f12b91034939248a4
JustLuuk <151666@csgaugustinus.nl>
2020-11-18 18:52:27 +0000

    veel

--- mijnDemoGame/JS/mijnGame.js
+++ mijnDemoGame/JS/mijnGame.js
@@ -1,25 +1,24 @@
 var achtergronden = [];
 
 function preload() {
-    //achtergrondmuziek = loadSound("sounds/Fade.mp3");
-    raket = loadImage("images/raket.png");
+    achtergrondmuziek = loadSound("sounds/saxophone.mp3");
     astro1 = loadImage("images/astro1.png");
     astro2 = loadImage("images/astro2.png");
     raketLangzaam = loadImage("images/raketLangzaam.png");
     raketNormaal = loadImage("images/raketNormaal.png");
     raketSnel = loadImage("images/raketSnel.png");
-    // Achtergonden op volghorde     
+    // Achtergonden op volgorde    
     achtergronden.push(loadImage("images/aarde-mars.png"));
-    achtergronden.push(loadImage("images/aarde-mars.png"));//zon
-    achtergronden.push(loadImage("images/aarde-mars.png"));//mercury
-    achtergronden.push(loadImage("images/aarde-mars.png"));//venus
-    achtergronden.push(loadImage("images/aarde-mars.png"));//aarde mars
-    achtergronden.push(loadImage("images/aarde-mars.png"));//jupiter
-    achtergronden.push(loadImage("images/saturnus.png"));//saturnes
-    achtergronden.push(loadImage("images/uranus.png"));//uranus
-    achtergronden.push(loadImage("images/Neptunes.png"));//neptunes
-    achtergronden.push(loadImage("images/aarde-mars.png"));//zwart gat
-    achtergronden.push(loadImage("images/aarde-mars.png"));//melkweg
+    achtergronden.push(loadImage("images/zon.png"));        //zon
+    achtergronden.push(loadImage("images/mercury.png"));    //mercury
+    achtergronden.push(loadImage("images/venus.png"));      //venus
+    achtergronden.push(loadImage("images/aarde-mars.png")); //aarde mars
+    achtergronden.push(loadImage("images/jupiter.png"));    //jupiter
+    achtergronden.push(loadImage("images/saturnus.png"));   //saturnes
+    achtergronden.push(loadImage("images/uranus.png"));     //uranus
+    achtergronden.push(loadImage("images/Neptunes.png"));   //neptunes
+    achtergronden.push(loadImage("images/zwart gat.png"));  //zwart gat
+    achtergronden.push(loadImage("images/melkweg.png"));    //melkweg
 }
 
 function windowResized() {
@@ -49,7 +48,7 @@ function keyTyped() {
   if (!spel.actief && !spel.levelGehaald) {
     // begin spel
     spel.actief = true;
-    //achtergrondmuziek.loop();
+    achtergrondmuziek.loop();
   }
   if ((spel.levelGehaald && !spel.afgelopen) && keyCode == ENTER) {
     // level gehaald tijdens het spel
@@ -57,7 +56,7 @@ function keyTyped() {
   }
   if ((spel.afgelopen) && keyCode == 32) {
     // einde spel => 32 = ENTER 
-    //achtergrondmuziek.stop();
+    achtergrondmuziek.stop();
     spel.nieuwSpel();
   }  
   }
